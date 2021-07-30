5f60d1e6632ef287febc7594597bc7f162d43b05
Tyrel Cadogan <shaqc777@yahoo.com>
2021-07-29 23:44:30 -0400

    updates (intro)

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-import "./intro.scss"
+import React from "react";
+import "./intro.scss";
+import { init} from 'ityped';
+import { useRef, useEffect } from "react";
+
 function Intro() {
-    return (
-        <div className="intro" id="intro">
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+          showCursor: false,
+          backDelay: 1500,
+          backSpeed: 60,
+          strings: ["Developer", "Electrical and Computer Engineering Student", "Freelancer"],
+        });
+
+    },[])
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Tyrel Cadogan</h1>
+          <h3>
+              <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
-export default Intro
+export default Intro;
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import "./portfolio.scss"
 export default  function Portfolio() {
-    return <div className="portfolio" id="portfolio"></div>;
+    return <div className="portfolio" id="portfolio">
+        <h1>Portfolio</h1>
+        <ul>
+            <li>Features</li>
+            <li>Web App</li>
+            <li>Computer Vision</li>
+            <li>Web Automation</li>
+            <li>IoT</li>
+        </ul>
+        <div className="container">
+            <div className="item">
+                
+            </div>
+        </div>
+    </div>;
 }
 
 
